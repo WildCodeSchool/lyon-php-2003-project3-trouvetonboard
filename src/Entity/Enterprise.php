62971ad7a92aaf876f2c3c27308a1bec0c37b650
Guillaume CAVALIE <actix2013@gmail.com>
2020-06-10 11:16:14 +0200

    MAKE ENTITY ENTERPRISE

---
+++ src/Entity/Enterprise.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EnterpriseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EnterpriseRepository::class)
+ */
+class Enterprise
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $paymentStatus;
+
+    /**
+     * @ORM\Column(type="string", length=300, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=300, nullable=true)
+     */
+    private $legelRepresentative;
+
+    /**
+     * @ORM\Column(type="string", length=300, nullable=true)
+     */
+    private $websiteLink;
+
+    /**
+     * @ORM\Column(type="string", length=300, nullable=true)
+     */
+    private $linkedinLink;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $zipCode;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPaymentStatus(): ?int
+    {
+        return $this->paymentStatus;
+    }
+
+    public function setPaymentStatus(int $paymentStatus): self
+    {
+        $this->paymentStatus = $paymentStatus;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getLegelRepresentative(): ?string
+    {
+        return $this->legelRepresentative;
+    }
+
+    public function setLegelRepresentative(?string $legelRepresentative): self
+    {
+        $this->legelRepresentative = $legelRepresentative;
+
+        return $this;
+    }
+
+    public function getWebsiteLink(): ?string
+    {
+        return $this->websiteLink;
+    }
+
+    public function setWebsiteLink(?string $websiteLink): self
+    {
+        $this->websiteLink = $websiteLink;
+
+        return $this;
+    }
+
+    public function getLinkedinLink(): ?string
+    {
+        return $this->linkedinLink;
+    }
+
+    public function setLinkedinLink(?string $linkedinLink): self
+    {
+        $this->linkedinLink = $linkedinLink;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(?int $zipCode): self
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20200610090956.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200610090956 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE enterprise (id INT AUTO_INCREMENT NOT NULL, payment_status INT NOT NULL, address VARCHAR(300) DEFAULT NULL, legel_representative VARCHAR(300) DEFAULT NULL, website_link VARCHAR(300) DEFAULT NULL, linkedin_link VARCHAR(300) DEFAULT NULL, zip_code INT DEFAULT NULL, city VARCHAR(45) DEFAULT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE enterprise');
+    }
+}
