a413fa7c174dd729903611cc87f3da7c09f75a56
Ben2669 <benjamin.moniotte@gmail.com>
2020-06-15 22:29:09 +0200

    redirection sur home à la vérification du mail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -97,6 +97,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_login');
+        return $this->redirectToRoute('home');
     }
 }
