77452281e7f02188d55e6c0466e90cc5572fe0ca
Ben2669 <benjamin.moniotte@gmail.com>
2020-06-18 00:17:39 +0200

    stylisation des messages d'erreurs sur le formulaire d'inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,7 +40,7 @@ class RegistrationController extends AbstractController
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
             $user->setPassword(
                 $passwordEncoder->encodePassword(
