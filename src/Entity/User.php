8ed85ca5a9ce71172ffaf00b00e5574f8dc77f92
Guillaume CAVALIE <actix2013@gmail.com>
2020-06-22 15:05:00 +0200

    Add if test for useremail

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -148,7 +148,7 @@ class User implements UserInterface
         return $this->id;
     }
 
-    public function getEmail(): ?string
+    public function getEmail(): string
     {
         return $this->email;
     }
