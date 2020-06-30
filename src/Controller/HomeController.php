44b1f74619cdd680f4aa2402baa9b68c56bb32f4
raphael-morales <raphael.morales@hotmail.fr>
2020-06-30 22:52:44 +0200

    Create templates inforEnterprise and infoAdvisor

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -19,5 +20,21 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route("/infoEnterprise", name="info_enterprise")
+     */
+    public function infoEnterprise() :Response
+    {
+        return $this->render('home/infoEnterprise.html.twig');
+    }
+
+    /**
+     * @Route("/infoAdvisor", name="info_advisor")
+     */
+    public function infoAdvisor() :Response
+    {
+        return $this->render('home/infoAdvisor.html.twig');
     }
 }
