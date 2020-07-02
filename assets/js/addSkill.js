70f4adc28f96fc7f1bf1218485fbf9a7f3259b48
Guillaume <actix2013@gmail.com>
2020-07-02 18:25:00 +0200

    js correction 125 error less

--- assets/js/addSkill.js
+++ assets/js/addSkill.js
@@ -3,30 +3,26 @@
 //console.log("__ Entering in addSkill JS file__")
 var debug = false;
 $(document).ready(() => {
-    if (debug) console.log('__ Add Skill __', 'Document ready');
-    if (debug) console.log('__ Add Skill __', 'recupération du  skill group');
-    //get all skill group , each skillGroup contain , hiden , label and checkbox
-    var skillBlocks = document.querySelectorAll('div[name="skillGroup"]');
-    if (debug) console.log('__ Add Skill __', skillBlocks);
-    //for each skillgroup  add event listener
+    // get all skill group , each skillGroup contain , hiden , label and checkbox
+    const skillBlocks = document.querySelectorAll('div[name="skillGroup"]');
+    // for each skillgroup  add event listener
     checkHasSkillAsynch(skillBlocks);
     for (var i = 0; i < skillBlocks.length; i++) {
-        //get one skillBlock witch it containt multiple elements
-        let aSkill = skillBlocks[i];
+        // get one skillBlock witch it containt multiple elements
+        const aSkill = skillBlocks[i];
         // get the checkbox in skillblock
-        let checkbox = aSkill.querySelector('input[name="skillCheckbox"]');
-        //get the skillid in hidden input
-        let linkAdd = aSkill.querySelector('#linkAdd');
-        let linkRemove = aSkill.querySelector('#linkRemove');
-        let linkCheck = aSkill.querySelector('#linkCheck');
-        if (debug) console.log('__Add Skill__','Profile add skill link :' + linkAdd.value);
-        //add event listener to the checkbox
+        const checkbox = aSkill.querySelector('input[name="skillCheckbox"]');
+        // get the skillid in hidden input
+        const linkAdd = aSkill.querySelector('#linkAdd');
+        const linkRemove = aSkill.querySelector('#linkRemove');
+        const linkCheck = aSkill.querySelector('#linkCheck');
+        // add event listener to the checkbox
         checkbox.addEventListener('change', function () {
-            //if checkbox is cheked
+            // if checkbox is cheked
             if (this.checked) {
-                //if (debug) console.log('__Add Skill__',
-                //'Skill ID :' + skillId.value + ' IS' + ' CHECKED');
-                //use fetch to  go  on url  linkadd.value
+                // if (debug) console.log('__Add Skill__',
+                // 'Skill ID :' + skillId.value + ' IS' + ' CHECKED');
+                // use fetch to  go  on url  linkadd.value
                 fetch(linkAdd.value).then(function (response) {
                     if (response.ok) {
                         if (debug)console.log('___ Add Skill ___', 'Then OK', response);
@@ -102,10 +98,7 @@ function checkHasSkill(skillBlocks) {
     }
 }
 
-/**
- *
- * @param skillBlocks
- */
+
 function checkHasSkillAsynch(skillBlocks) {
     for (var i = 0; i < skillBlocks.length; i++) {
         //get one skillBlock witch it containt multiple elements
@@ -122,7 +115,6 @@ function checkHasSkillAsynch(skillBlocks) {
 }
 
 function setCheckBox(link,checkbox)  {
-
     try{
         let datas = null;
         return fetch(link)
@@ -141,9 +133,7 @@ function setCheckBox(link,checkbox)  {
                 return datas;
             })
         //console.log('___ Add skill Data asynch___', datas);
-
         } catch (error) {
             console.log('___ Add skill ___', ' Serveur error no repsonse in skill Asych function control');
         }
-
 }
