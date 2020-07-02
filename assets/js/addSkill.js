096b7f617e775eee40cfe247d6a00e31e12ffe18
Guillaume CAVALIE <actix2013@gmail.com>
2020-07-02 20:44:03 +0200

    clean jS step 2

--- assets/js/addSkill.js
+++ assets/js/addSkill.js
@@ -1,71 +1,6 @@
 // get all  div where is used for a skill
-
-console.log("__ Entering in addSkill JS file__")
-var debug = false;
-$(document).ready(() => {
-    // get all skill group , each skillGroup contain , hiden , label and checkbox
-    const skillBlocks = document.querySelectorAll('div[name="skillGroup"]');
-    //console.log("___ Add Skill ___" , skillBlocks);
-    checkHasSkillAsynch(skillBlocks);
-    // for each skillgroup  add event listener
-    for (var i = 0; i < skillBlocks.length; i++) {
-        // get one skillBlock witch it containt multiple elements
-        const aSkill = skillBlocks[i];
-        //console.log("___ Add Skill ___", aSkill);
-        // get the checkbox in skillblock
-        const checkbox = aSkill.querySelector('input[name="skillCheckbox"]');
-        // get the skillid in hidden input
-        const linkAdd = aSkill.querySelector('#linkAdd');
-        const linkRemove = aSkill.querySelector('#linkRemove');
-        const linkCheck = aSkill.querySelector('#linkCheck');
-        // add event listener to the checkbox
-        checkbox.addEventListener('change', function () {
-            // if checkbox is cheked
-            if (this.checked) {
-                // use fetch to  go  on url  linkadd.value
-                console.log("__ Add skill link__", linkAdd)
-                fetch(linkAdd.value).then(function (response) {
-                    if (response.ok) {
-                        if (debug) console.log('___ Add Skill ___', 'Then OK', response);
-                        response.json().then(function (data) {
-                            if (data.isChecked) {
-                                //checkbox.style.borderColor = "green";
-                                //checkbox.style.borderWidth = "1px";
-                                //checkbox.style.borderStyle = "solid";
-                            }
-                        });
-                    } else {
-                        console.log('Mauvaise réponse du réseau');
-                    }
-                })
-                    .catch(function (error) {
-                        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-                    });
-            } else {
-                fetch(linkRemove.value).then(function (response) {
-                    console.log("__ Add skill link remove__", linkRemove.value)
-                    if (response.ok) {
-                        //console.log("___ Add Skill ___", "Then OK", response);
-                        response.json().then(function (data) {
-                            if (!data.isChecked) {
-                                //todo implement errors possibility
-                                //checkbox.style.borderColor = "red";
-                                //checkbox.style.borderWidth = "1px";
-                                //checkbox.style.borderStyle = "solid";
-                            } else {
-
-                            }
-                        });
-                    }
-                })
-                    .catch(function (error) {
-                        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-                    });
-            }
-        });
-    }
-});
-
+//console.log("__ Entering in addSkill JS file__")
+const debug = false;
 
 function checkHasSkillAsynch(skillBlocks) {
     for (var i = 0; i < skillBlocks.length; i++) {
@@ -104,3 +39,62 @@ async function setCheckBox(link, checkbox) {
         console.log('___ Add skill ___', ' Serveur error no repsonse in skill Asych function control');
     }
 }
+
+$(document).ready(() => {
+    // get all skill group , each skillGroup contain , hiden , label and checkbox
+    const skillBlocks = document.querySelectorAll('div[name="skillGroup"]');
+    // console.log("___ Add Skill ___" , skillBlocks);
+    checkHasSkillAsynch(skillBlocks);
+    // for each skillgroup  add event listener
+    for (var i = 0; i < skillBlocks.length; i++) {
+        // get one skillBlock witch it containt multiple elements
+        const aSkill = skillBlocks[i];
+        // console.log('___ Add Skill ___', aSkill);
+        // get the checkbox in skillblock
+        const checkbox = aSkill.querySelector('input[name="skillCheckbox"]');
+        // get the skillid in hidden input
+        const linkAdd = aSkill.querySelector('#linkAdd');
+        const linkRemove = aSkill.querySelector('#linkRemove');
+        const linkCheck = aSkill.querySelector('#linkCheck');
+        // add event listener to the checkbox
+        checkbox.addEventListener('change', function () {
+            // if checkbox is cheked
+            if (this.checked) {
+                // use fetch to  go  on url  linkadd.value
+                console.log("__ Add skill link__", linkAdd)
+                fetch(linkAdd.value).then(function (response) {
+                    if (response.ok) {
+                        if (debug) console.log('___ Add Skill ___', 'Then OK', response);
+                        response.json().then(function (data) {
+                            if (data.isChecked) {
+                                //todo implement errors possibility
+                            }
+                        });
+                    } else {
+                        console.log('Mauvaise réponse du réseau');
+                    }
+                })
+                    .catch(function (error) {
+                        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+                    });
+            } else {
+                fetch(linkRemove.value).then(function (response) {
+                    console.log("__ Add skill link remove__", linkRemove.value)
+                    if (response.ok) {
+                        //console.log("___ Add Skill ___", "Then OK", response);
+                        response.json().then(function (data) {
+                            if (!data.isChecked) {
+                                //todo implement errors possibility
+                            }
+                        });
+                    }
+                })
+                    .catch(function (error) {
+                        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+                    });
+            }
+        });
+    }
+});
+
+
