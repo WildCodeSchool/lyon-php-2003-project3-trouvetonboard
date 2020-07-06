0e263378b34936c196443eb5be164b4ca0790349
Ben2669 <benjamin.moniotte@gmail.com>
2020-07-06 11:17:37 +0200

    add use Vich in both Advisor and Enterprise entities

--- src/Entity/Advisor.php
+++ src/Entity/Advisor.php
@@ -6,6 +6,7 @@ use App\Repository\AdvisorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Vich\UploaderBundle\Entity\File;
 
 /**
--- src/Entity/Enterprise.php
+++ src/Entity/Enterprise.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Vich\UploaderBundle\Entity\File;
 
 /**
