b805dd91400da4f890571138c5498e69262842bd
Guillaume CAVALIE <actix2013@gmail.com>
2020-07-07 20:18:03 +0200

    request test advisor

--- src/Repository/ProfileRepository.php
+++ src/Repository/ProfileRepository.php
@@ -25,38 +25,22 @@ class ProfileRepository extends ServiceEntityRepository
 
     public function findAdvisorMatchsByBoardRequest(int $id)
     {
+        $rawSql = "SELECT PE.title, UA.id, UA.first_name, UA.last_name,
+        COUNT(DISTINCT PSA.skill_id) as SCORE
+        FROM profile PE
+        JOIN profile_skill PSE ON PSE.profile_id = PE.id
+        JOIN profile_skill PSA ON PSA.skill_id = PSE.skill_id
+        JOIN profile PA ON PA.id = PSA.profile_id
+        JOIN advisor A ON PA.advisor_id = A.id
+        JOIN user UA ON UA.advisor_id = A.id
+        WHERE
+        PE.id = $id
+        GROUP BY UA.id
+        ORDER BY SCORE DESC";
 
-//        $qb = $this->createQueryBuilder('PE')
-//            ->select('PE.title, UA.id, UA.first_name, UA.last_name,
-//            COUNT(DISTINCT PSA.skill_id) as SCORE')
-//            ->join('PE.skills', 'PSE')
-//            ->join('profile_skill', 'PSA', 'ON', 'PSA.skill_id = PSE.skill_id')
-//            ->join('profile', 'PA', 'ON', 'PA.id = PSA.profile_id')
-//            ->join('advisor', 'A', 'ON', 'PA.advisor_id = A.id')
-//            ->join('user', 'UA', 'ON', 'UA.advisor_id = A.id')
-//            ->andWhere("PE.id = $id")
-//            ->groupBy('UA.id')
-//            ->orderBy('SCORE DESC');
-//        $query = $qb->getQuery();
-
-//        $entityManager = $this->getEntityManager();
-//        $rq = "SELECT PE.title, UA.id, UA.first_name, UA.last_name,
-//        COUNT(DISTINCT PSA.skill_id) as SCORE
-//        FROM profile PE
-//        JOIN profile_skill PSE ON PSE.profile_id = PE.id
-//        JOIN profile_skill PSA ON PSA.skill_id = PSE.skill_id
-//        JOIN profile PA ON PA.id = PSA.profile_id
-//        JOIN advisor A ON PA.advisor_id = A.id
-//        JOIN user UA ON UA.advisor_id = A.id
-//        WHERE
-//        PE.id = $id
-//        GROUP BY UA.id
-//        ORDER BY SCORE DESC";
-
-//        $query = $entityManager->createNativeQuery($rq);
-
-
-//        return $query->getResult();
+        $stmt = $this->getEntityManager()->getConnection()->prepare($rawSql);
+        $stmt->execute([]);
+        return $stmt->fetchAll();
     }
 
 
