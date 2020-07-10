4a283f52442fcb39919c69b15db7fc28b02029b6
Guillaume CAVALIE <actix2013@gmail.com>
2020-07-10 10:48:52 +0200

    Travis JS scss

--- assets/js/enterpriseMatch.js
+++ assets/js/enterpriseMatch.js
@@ -1,10 +1,9 @@
 /* global $ */
 $(document).ready(() => {
     const boardRequestActive = $('div[name = "boardRequestActive"]');
-    //console.log("bord request active : ", boardRequestActive);
     const divConstainAllCategory = $('#allBoardRequest');
     divConstainAllCategory.children().removeClass('bg-white border-bottom border-top  bg-color-styleGuideGray')
-             .addClass('bg-tranparent');
+        .addClass('bg-tranparent');
     boardRequestActive.removeClass('bg-light border-right')
-             .addClass('bg-white border-style-blue border-bottom border-top  bg-color-styleGuideGray');
+        .addClass('bg-white border-style-blue border-bottom border-top  bg-color-styleGuideGray');
 });
