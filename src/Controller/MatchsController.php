d790690a09f6e025a18b223d0a19fd4c953fc9c7
Guillaume CAVALIE <actix2013@gmail.com>
2020-07-17 10:45:32 +0200

    add select in profilerepository for include total  skills request by matchs

--- src/Controller/MatchsController.php
+++ src/Controller/MatchsController.php
@@ -262,8 +262,6 @@ class MatchsController extends AbstractController
     {
 
         $logUser = $this->getUser();
-
-
         $email = (new TemplatedEmail())
             ->from($this->getParameter("mailer_from"))
             ->to($this->getParameter("mailer_cedric"))
