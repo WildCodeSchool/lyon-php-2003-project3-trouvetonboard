1d9914425629ee608ea3f0332778c4fd5f7fc832
Ben2669 <benjamin.moniotte@gmail.com>
2020-07-17 10:47:08 +0200

    resolving errors issues from travis in CVModal.js

--- assets/js/CVModal.js
+++ assets/js/CVModal.js
@@ -1,25 +1,27 @@
 function fade(element) {
+    const elementToChange = element;
     let op = 1;// initial opacity
     const timer = setInterval(() => {
         if (op <= 0.1) {
             clearInterval(timer);
-            element.style.display = 'none';
+            elementToChange.style.display = 'none';
         }
-        element.style.opacity = op;
-        element.style.filter = `alpha(opacity=${op * 100})`;
+        elementToChange.style.opacity = op;
+        elementToChange.style.filter = `alpha(opacity=${op * 100})`;
         op -= op * 0.1;
     }, 5);
 }
 
 function unfade(element) {
+    const elementToChange = element;
     let op = 0.1;// initial opacity
-    element.style.display = 'block';
+    elementToChange.style.display = 'block';
     const timer = setInterval(() => {
         if (op >= 1) {
             clearInterval(timer);
         }
-        element.style.opacity = op;
-        element.style.filter = `alpha(opacity=${op * 100})`;
+        elementToChange.style.opacity = op;
+        elementToChange.style.filter = `alpha(opacity=${op * 100})`;
         op += op * 0.1;
     }, 10);
 }
