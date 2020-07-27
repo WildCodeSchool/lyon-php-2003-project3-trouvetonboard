675cc2372ca8c1de38e10a4b1f13a16f986625cd
Ben2669 <benjamin.moniotte@gmail.com>
2020-07-27 10:50:11 +0200

    removing useless uploadedFileValue.js

--- assets/js/uploadedFileValue.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line func-names
-document.getElementById('advisor_cvLinkFile_file').onchange = function () {
-    document.getElementById('advisor_cvLinkFile_file').value = this.value;
-};
-
-// eslint-disable-next-line func-names
-document.getElementById('enterprise_brochureFile_file').onchange = function () {
-    document.getElementById('enterprise_brochureFile_file').value = this.value;
-};
